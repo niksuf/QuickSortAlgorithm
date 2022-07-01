6043ff5ff7d935c853eaab300bad127dd5105295
Nikita Sufyanov <niksuf1@yandex.ru>
2022-07-01 03:38:56 +0300

    Code format fix

--- QuickSort.py
+++ QuickSort.py
@@ -1,13 +1,15 @@
 from random import random, seed, randint
 from time import time
 
-def isSorted(array, size):                                  # Проверка на упорядоченность
+
+def isSorted(array, size):                                      # Проверка на упорядоченность
     for i in range (0, size - 1):
         if array[i] > array[i + 1]:
             return False
     return True
 
-def printArray(array, size):                                # Вывод массива
+
+def printArray(array, size):                                    # Вывод массива
     if isSorted(array, size):
         print('Sorted!')
     else:
@@ -17,7 +19,8 @@ def printArray(array, size):                                # Вывод мас�
         else: break
     print('\n')
 
-def sort(array, start, end):                                # Сортировка массива
+
+def sort(array, start, end):                                    # Сортировка массива
     if start <= end:
         mid = array[randint(start, end)]
         i = start
@@ -32,21 +35,27 @@ def sort(array, start, end):                                # Сортировк
         sort(array, start, j)
         sort(array, i, end)
 
-seed(time())                                                # Инициализация таймера
-print('Input array size: ')
-size = int(input())                                         # Размер массива
-random_array = []
-for i in range (0, size):                                   # Заполнение массива случайными числами
-    random_array.append(random() * size)
-printArray(random_array, size)
 
-l = 0
-r = size - 1
+def main():
+    seed(time())                                                # Инициализация таймера
+    print('Input array size: ')
+    size = int(input())                                         # Размер массива
+    random_array = []
+    for i in range (0, size):                                   # Заполнение массива случайными числами
+        random_array.append(random() * size)
+    printArray(random_array, size)
 
-t1 = time()
-sort(random_array, l, r)                                    # Сортировка
-t2 = time()
-elapsed = 1000 * (t2-t1)                                    # Время, затраченное на операцию, мс
+    l = 0
+    r = size - 1
 
-printArray(random_array, size)
-print('Time: ', elapsed, 'ms')
+    t1 = time()
+    sort(random_array, l, r)                                    # Сортировка
+    t2 = time()
+    elapsed = 1000 * (t2-t1)                                    # Время, затраченное на операцию, мс
+
+    printArray(random_array, size)
+    print('Time: ', elapsed, 'ms')
+
+
+if __name__ == '__main__':
+    main()
